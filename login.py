8b65ce56feba5634e68d21dc5973c9aa6d2629e4
Mloser <82438049+Mloser-z@users.noreply.github.com>
2022-01-01 15:22:04 +0800

    Add files via upload
    历史记录爬取完成

---
+++ login.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+# 登录
+def login():
+    web = webdriver.Chrome(executable_path="D:/chromedriver/chromedriver_version_96/chromedriver.exe")
+    web.get(url="https://passport.bilibili.com/login")
+    input("登录成功后再输入任意字符继续")
+    return web
+
+
+# 点击历史记录界面
+def get_history_page(web):
+    history_button = web.find_element(By.CLASS_NAME, "mini-history")
+    history_button.click()
+    time.sleep(3)
+    # 切换至新窗口
+    windows = web.window_handles
+    web.switch_to.window(windows[1])
+    return web
+
+
+# 下拉滚动条并且返回源码
+def get_text(web):
+    status = True
+    height = web.execute_script("return action=document.body.scrollHeight")
+    web.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(0.5)
+
+    while status:
+        new_height = web.execute_script("return action=document.body.scrollHeight")
+        if new_height > height:
+            web.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+            time.sleep(0.5)
+            height = new_height
+        else:
+            status = False
+
+    with open(file="history.html", mode="w", encoding="utf-8") as file:
+        file.write(web.page_source)
+    return web.page_source
+
+
+def start():
+    web_d = login()
+    get_history_page(web_d)
+    get_text(web_d)
---
+++ spider.py
@@ -0,0 +1,90 @@
+import re
+from bs4 import BeautifulSoup
+import json
+import login
+
+
+def get_user_name(sp):
+    name = sp.find(class_="username")
+    label = sp.find(class_="label")
+    if name is not None:
+        return name.string
+    elif label is not None:
+        return label.string
+    else:
+        return None
+
+
+def get_time(sp):
+    time = sp.find(class_="lastplay-t")
+    return time.string
+
+
+def get_video_title(sp):
+    title = sp.find(class_="title")
+    subtitle = sp.find(class_="subtitle")
+    if subtitle.string is None:
+        return title.string.replace("\n", "").replace(" ", "")
+    else:
+        return title.string.replace("\n", " ").replace(" ", "") + ':' + subtitle.string.replace("\n", " ").replace(" ", "")
+
+
+def get_video_type(sp):
+    video_type = sp.find(class_="name")
+    label = sp.find(class_="label")
+    if video_type is not None:
+        return video_type.string
+    elif label is not None:
+        return label.string
+    else:
+        return None
+
+
+def get_device_type(sp):
+    wrap_i = sp.find(class_="time-wrap").i.get('class')
+    device_type = wrap_i[2][5:]
+    return device_type
+
+
+def is_repeat(infor_dic, infor_ed_dic):
+    if infor_dic.get("title") == infor_ed_dic.get("title") and infor_dic.get("up_name") == infor_ed_dic.get("up_name"):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    login.start()
+    file_name = "history.html"
+    soup = BeautifulSoup(open(file_name, encoding="utf-8"), "lxml")
+    lis = soup.find_all(class_="history-record")
+    infor_lists = []
+    infor_ed_lists = []
+    file_name = "history.json"
+    
+    # 读取文件，导出已储存信息，用来判断是否重复
+    try:
+        with open(file_name, "r", encoding="utf-8") as rp:
+            infor_ed_lists = json.load(rp)
+    except Exception:
+        print("No File")
+
+    with open(file_name, "w", encoding="utf-8") as fp:
+        n = 0
+        for li in lis:
+            sp = BeautifulSoup(str(li), "lxml")
+            infor = {"title": get_video_title(sp), "up_name": get_user_name(sp), "video_type": get_video_type(sp), "device": get_device_type(sp), "time": get_time(sp)}
+            # 只爬取两天前的数据
+            if re.match(re.compile(r'\d\d:\d\d'), infor.get("time")) is not None:
+                continue
+            if infor in infor_ed_lists:
+                break
+            else:
+                infor_lists.append(infor)
+                n += 1
+        infor_lists.extend(infor_ed_lists)
+        json.dump(infor_lists, fp, ensure_ascii=False)
+        print("新增数据: ", n)
+
+    for infor in infor_lists:
+        print(infor)
